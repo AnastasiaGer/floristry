e4fa91839807c24b8981187b97c5d6fec8623483
Anisa <aagermanova@gmail.com>
2020-09-28 15:02:27 -0700

    PP

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+$(function () {
+  // $('.about__slider, .trip__slider').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../img/arrow-prew.svg" alt=""></button>',
+  //   nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../img/arrow-next.svg" alt=""></button>',
+  //   autoplay: true,
+  //   fade: true,
+  //   responsive: [{
+  //     breakpoint: 601,
+  //     settings: {
+  //       arrows: false
+  //     }
+  //   }]
+  // });
+
+  // $('.menu').on('click', 'a', function (event) {
+  //   event.preventDefault();
+  //   let id = $(this).attr('href'),
+  //   top = $(id).offset().top;
+
+  //   $('body,html').animate({scrollTop: top}, 1500);
+
+  // });
+});
