88dbcc33d23808ddf606e57bf98a734b11e25e02
Anisa <aagermanova@gmail.com>
2020-09-28 21:08:31 -0700

    header CSS

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,15 @@
 $(function () {
+  $('.menu__btn').on('click', function(){
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__list-link').on('click', function(){
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
+  });
+
+
   // $('.catalog__list').slick({
   //   infinite: true,
   //   fade: true,
