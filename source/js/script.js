b631476c1eeaa482fbfbe6485327d908051197e9
Anisa <aagermanova@gmail.com>
2020-10-01 16:11:24 -0700

    slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,55 +1,6 @@
 $(function () {
-  $('.menu__btn').on('click', function(){
-    $('.menu__btn').toggleClass('menu__btn--active');
-    $('.menu__list-wrapper').toggleClass('menu__list-wrapper--active');
+  $('.photos-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
   });
-
-  $('.menu__list-link').on('click', function(){
-    $('.menu__btn').removeClass('menu__btn--active');
-    $('.menu__list-wrapper').removeClass('menu__list-wrapper--active');
-  });
-
-
-  // $('.catalog__list').slick({
-  //   infinite: true,
-  //   fade: true,
-  //   prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-  //   nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-  //   asNavFor: '.slider-dotshead'
-  // });
-
-  // $('.slider-dotshead').slick({
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  //   asNavFor: '.header__slider',
-  //   responsive: [
-  //     {
-  //         breakpoint: 961,
-  //             settings: "unslick"
-  //     }
-  // ]
-  // });
-  // $('.about__slider, .trip__slider').slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../img/arrow-prew.svg" alt=""></button>',
-  //   nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../img/arrow-next.svg" alt=""></button>',
-  //   autoplay: true,
-  //   fade: true,
-  //   responsive: [{
-  //     breakpoint: 601,
-  //     settings: {
-  //       arrows: false
-  //     }
-  //   }]
-  // });
-
-  // $('.menu').on('click', 'a', function (event) {
-  //   event.preventDefault();
-  //   let id = $(this).attr('href'),
-  //   top = $(id).offset().top;
-
-  //   $('body,html').animate({scrollTop: top}, 1500);
-
-  // });
 });
