d3fef72235a066afd11ed052ba2778dc26147f0c
Anisa <aagermanova@gmail.com>
2020-10-02 13:27:44 -0700

    catalog part1

--- source/js/script.js
+++ source/js/script.js
@@ -2,5 +2,7 @@ $(function () {
   $('.photos-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
+    prevArrow: '<button class="photos-slider__btn photos-slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
+    nextArrow: '<button class="photos-slider__btn photos-slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
   });
 });
